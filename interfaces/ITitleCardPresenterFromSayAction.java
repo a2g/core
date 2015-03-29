b137bbe69da8d215017bf9ce8137bd0817871ed6
anthony <githuba2g@antcassidy.com>
2015-03-29 19:06:46 +1300

    added missing items

---
+++ interfaces/ITitleCardPresenterFromSayAction.java
@@ -0,0 +1,9 @@
+package com.github.a2g.core.interfaces;
+
+import com.github.a2g.core.interfaces.action.IGetPopupDisplayDuration;
+
+public interface ITitleCardPresenterFromSayAction extends
+IGetPopupDisplayDuration
+{
+	void displayTitleCard(String text);
+}
--- objectmodel/InventoryPanel.java
+++ objectmodel/InventoryPanel.java
@@ -19,93 +19,98 @@ import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.LoadHandler;
-import com.github.a2g.core.interfaces.ImagePanelAPI;
 import com.github.a2g.core.interfaces.IInventoryPanelFromInventoryPresenter;
+import com.github.a2g.core.interfaces.IInventoryPresenterFromInventoryMouseOver;
 import com.github.a2g.core.interfaces.IInventoryPresenterFromInventoryPanel;
 import com.github.a2g.core.interfaces.IPackagedImage;
+import com.github.a2g.core.interfaces.ImagePanelAPI;
 import com.github.a2g.core.platforms.html4.ImageForHtml4;
 import com.github.a2g.core.platforms.html4.PackagedImageForHtml4;
 import com.github.a2g.core.platforms.html4.mouse.InventoryItemMouseClickHandler;
 import com.github.a2g.core.platforms.html4.mouse.InventoryItemMouseOverHandler;
 import com.github.a2g.core.primitive.ColorEnum;
 import com.github.a2g.core.primitive.Point;
+import com.github.a2g.core.res.UserInterfaceDecoration;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 
-public class InventoryPanel extends AbsolutePanel implements ImagePanelAPI,
-		IInventoryPanelFromInventoryPresenter {
+public class InventoryPanel
+extends AbsolutePanel
+implements
+ImagePanelAPI
+, IInventoryPanelFromInventoryPresenter
+{
 	final FlowPanel arrowLeft;
 	final FlowPanel arrowRight;
 	final IInventoryPresenterFromInventoryPanel mouseToPresenter;
 
-	public InventoryPanel(
-			final IInventoryPresenterFromInventoryPanel mouseToPresenter,
-			ColorEnum fore, ColorEnum back, ColorEnum rollover) {
+	public InventoryPanel(final IInventoryPresenterFromInventoryPanel mouseToPresenter, ColorEnum fore, ColorEnum back, ColorEnum rollover)
+	{
 		this.mouseToPresenter = mouseToPresenter;
 
 		getElement().getStyle().setProperty("color", fore.toString());
 		getElement().getStyle().setProperty("backgroundColor", back.toString());
-
+				
 		arrowLeft = new FlowPanel();
 		this.add(arrowLeft);
 		arrowLeft.getElement().addClassName("arrowLeft");
-
+		
+		
 		arrowLeft.setHeight("0");
 		arrowLeft.setWidth("0");
-		arrowLeft.getElement().getStyle()
-				.setProperty("borderBottom", "10px solid transparent");
-		arrowLeft.getElement().getStyle()
-				.setProperty("borderRight", "10px solid " + fore.toString());
-		arrowLeft.getElement().getStyle()
-				.setProperty("borderTop", "10px solid transparent");
+		arrowLeft.getElement().getStyle().setProperty("borderBottom", "10px solid transparent");
+		arrowLeft.getElement().getStyle().setProperty("borderRight", "10px solid "+fore.toString());
+		arrowLeft.getElement().getStyle().setProperty("borderTop", "10px solid transparent");
 		arrowLeft.getElement().getStyle().setProperty("float", "left");
 		arrowLeft.getElement().getStyle().setPosition(Position.RELATIVE);
 		arrowLeft.getElement().getStyle().setProperty("top", "50%");
-		arrowLeft.getElement().getStyle()
-				.setProperty("transform", "translateY(-50%)");
-
+		arrowLeft.getElement().getStyle().setProperty("transform", "translateY(-50%)");
+		
 		arrowRight = new FlowPanel();
 		this.add(arrowRight);
 		arrowRight.getElement().addClassName("arrowRight");
-
+		
+		
 		arrowRight.setHeight("0");
 		arrowRight.setWidth("0");
-		arrowRight.getElement().getStyle()
-				.setProperty("borderBottom", "10px solid transparent");
-		arrowRight.getElement().getStyle()
-				.setProperty("borderLeft", "10px solid " + fore.toString());
-		arrowRight.getElement().getStyle()
-				.setProperty("borderTop", "10px solid transparent");
+		arrowRight.getElement().getStyle().setProperty("borderBottom", "10px solid transparent");
+		arrowRight.getElement().getStyle().setProperty("borderLeft", "10px solid "+fore.toString());
+		arrowRight.getElement().getStyle().setProperty("borderTop", "10px solid transparent");
 		arrowRight.getElement().getStyle().setProperty("float", "right");
-
+		
 		arrowRight.getElement().getStyle().setPosition(Position.RELATIVE);
 		arrowRight.getElement().getStyle().setProperty("top", "50%");
-		arrowRight.getElement().getStyle()
-				.setProperty("transform", "translateY(-50%)");
-
-		arrowLeft.addDomHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				mouseToPresenter.setMouseOver(.05, .5);
-				mouseToPresenter.doClick();
-			}
-		}, ClickEvent.getType());
-		arrowRight.addDomHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				mouseToPresenter.setMouseOver(.95, .5);
-				mouseToPresenter.doClick();
-			}
-
-		}, ClickEvent.getType());
-		this.setVisible(true);
-
+		arrowRight.getElement().getStyle().setProperty("transform", "translateY(-50%)");
+		
+		arrowLeft.addDomHandler(
+				new ClickHandler()
+				{	@Override
+					public void onClick(ClickEvent event)
+				{
+					mouseToPresenter.setMouseOver(.05, .5);
+					mouseToPresenter.doClick();
+				}
+				}, ClickEvent.getType());
+		arrowRight.addDomHandler(
+				new ClickHandler()
+				{	@Override
+					public void onClick(ClickEvent event)
+				{
+					mouseToPresenter.setMouseOver(.95, .5);
+					mouseToPresenter.doClick();
+				}
+				
+				}, ClickEvent.getType());
+		
 	}
 
 	@Override
-	public void updateInventory(Inventory inventory) {
+	public void updateInventory(Inventory inventory)
+	{
 	}
+
 
 	@Override
 	public Image createNewImageAndAdddHandlers(IPackagedImage imageResource,
@@ -115,12 +120,11 @@ public class InventoryPanel extends AbsolutePanel implements ImagePanelAPI,
 		com.google.gwt.user.client.ui.Image image = Image.getImageFromResource(
 				(PackagedImageForHtml4) imageResource, lh);
 
-		ImageForHtml4 imageAndPos = new ImageForHtml4(image, this, new Point(0,
-				0));
+		ImageForHtml4 imageAndPos = new ImageForHtml4(image, this, new Point(0, 0));
 
 		imageAndPos.getNativeImage().addMouseMoveHandler(
-				new InventoryItemMouseOverHandler(bus, mouseToPresenter,
-						objectTextualId, objectCode));
+				new InventoryItemMouseOverHandler(bus, mouseToPresenter, objectTextualId,
+						objectCode));
 
 		imageAndPos.getNativeImage().addClickHandler(
 				new InventoryItemMouseClickHandler(this, mouseToPresenter));
@@ -132,7 +136,7 @@ public class InventoryPanel extends AbsolutePanel implements ImagePanelAPI,
 	public void setImageVisible(Image image, boolean visible) {
 
 		super.setVisible(((ImageForHtml4) image).getNativeImage().getElement(),
-			true);
+				visible);
 	}
 
 	@Override
@@ -152,8 +156,7 @@ public class InventoryPanel extends AbsolutePanel implements ImagePanelAPI,
 
 	@Override
 	public void setThingPosition(Image image, int left, int top) {
-		super.setWidgetPosition(((ImageForHtml4) image).getNativeImage(), left,
-				top);
+		super.setWidgetPosition(((ImageForHtml4) image).getNativeImage(), left, top);
 	}
 
 	@Override
@@ -166,12 +169,10 @@ public class InventoryPanel extends AbsolutePanel implements ImagePanelAPI,
 		return ((ImageForHtml4) image).getNativeImage().getWidth();
 	}
 
-	@Override
 	public void setLeftArrowVisible(boolean visible) {
 		arrowLeft.setVisible(visible);
 	}
 
-	@Override
 	public void setRightArrowVisible(boolean visible) {
 		arrowRight.setVisible(visible);
 
@@ -179,13 +180,11 @@ public class InventoryPanel extends AbsolutePanel implements ImagePanelAPI,
 
 	@Override
 	public void setScenePixelSize(int width, int height) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub	
 	}
 
 	@Override
 	public void setInventoryImageSize(int width, int height) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub	
 	}
 }
