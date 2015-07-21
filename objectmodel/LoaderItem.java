67d96d71008f8fe3e4f6e659bd1de3071991cd35
Anthony Cassidy <githuba2g@antcassidy.com>
2015-07-22 00:18:22 +1200

    Fixed loader so they always load "main" first.

--- objectmodel/LoaderItem.java
+++ objectmodel/LoaderItem.java
@@ -95,6 +95,10 @@ public class LoaderItem implements LoadHandler, Comparable<LoaderItem> {
 
 	@Override
 	public int compareTo(LoaderItem o) {
+		int thisMain = this.getName().contains("main")? 1:0;
+		int thatMain = o.getName().contains("main")? 1:0;
+		if(thisMain!=thatMain)
+			return thatMain - thisMain;
 		int result = this.bundleNumber - o.bundleNumber;
 		return result;
 	}
