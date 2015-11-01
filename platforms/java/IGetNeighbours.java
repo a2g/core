33448acf47b0f8dcb54ca05b163ee1b313d219f9
Anthony Cassidy <githuba2g@antcassidy.com>
2015-11-01 21:50:10 +1300

    addde eric lipperts c# code

---
+++ platforms/java/IGetNeighbours.java
@@ -0,0 +1,8 @@
+package com.github.a2g.core.platforms.java;
+
+import java.util.List;
+
+interface IGetNeighbours<N>  
+{
+	List<N> GetNeighbours();
+}
---
+++ platforms/java/PathIterator.java
@@ -0,0 +1,26 @@
+package com.github.a2g.core.platforms.java;
+
+import java.util.Iterator;
+
+public class PathIterator<T> implements Iterator<T> {
+
+	@Override
+	public boolean hasNext() {
+		
+		return false;
+	}
+
+	@Override
+	public T next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	   
+}
