27496a3ff24656d8569529f61f8e70361cb00d03
Anthony Cassidy <githuba2g@antcassidy.com>
2015-12-27 21:57:16 +1100

    aded another dialog constant, for convenience

--- interfaces/ConstantsForAPI.java
+++ interfaces/ConstantsForAPI.java
@@ -63,6 +63,7 @@ public interface ConstantsForAPI {
 	 * -1 should never be used as a magic number
 	 */
 	public static final int EXIT_DLG = -1;
+	public static final int ROOT_DLG = 0;
 
 	public static enum Special {
 		North(1), East(2), South(3), West(4);
