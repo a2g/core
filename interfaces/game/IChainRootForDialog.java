9421be7e47a82e4b8a414ff159193fd8b32881fd
Anthony Cassidy <githuba2g@antcassidy.com>
2018-08-07 00:43:19 +1200

    Added doxygen docs
    Signed-off-by: Anthony Cassidy <githuba2g@antcassidy.com>

--- interfaces/game/IChainRootForDialog.java
+++ interfaces/game/IChainRootForDialog.java
@@ -14,27 +14,31 @@ public interface IChainRootForDialog extends IChainRootCommon
 , ISetInventoryItemVisible
 , ISetAnimationAsObjectInitial
 {
-	// Start with dialog only stuff
+	/*@ Start with dialog only stuff */
+	//@{
 	DialogChainableAction them(String speech);
 	DialogChainableAction us(String speech);
 	DialogChainableAction branchNormal(int branchId, final boolean isOkToAdd, String text);
 	DialogChainableAction branchNormal(int branchId, String text) ;
 	DialogChainableAction branchSticky(int branchId, String text) ;
 	DialogChainableAction branchSticky(int nativ2, boolean b, String string);
+	//@}
 	
-	// include a bunch of methods also found on the scene chains....
+	/*@ include a bunch of methods also found on the scene chains.... */
+	//@{
 	@Override DialogChainableAction setValue(Object key, int value) ;
 	@Override DialogChainableAction setInventoryItemVisible(int redNote, boolean b);
 	@Override DialogChainableAction setAnimationAsObjectInitial(String atid);
 	@Override DialogChainableAction sleep(int milliseconds);
+	//@}
 
-	// ..and a whole heap of dialog specific exits
+	/*@ ..and a whole heap of dialog specific exits */
+	//@{
 	DialogChainEndAction endDialogTree();
 	DialogChainEndAction chainTo(int branchId);
 	DialogChainEndAction switchTo(String string, int arrivalSegment);
 	DialogChainEndAction branchTheObligatoryExit(String string);
-	DialogChainEndAction walkAlwaysSwitch(Point p, String sceneName,
-			int arrivalSegment);
-	DialogChainEndAction walkAlwaysSwitch(double x, double y, String sceneName,
-			int arrivalSegment);
+	DialogChainEndAction walkAlwaysSwitch(Point p, String sceneName, int arrivalSegment);
+	DialogChainEndAction walkAlwaysSwitch(double x, double y, String sceneName,	int arrivalSegment);
+	//@}
 }
--- interfaces/game/IChainRootForScene.java
+++ interfaces/game/IChainRootForScene.java
@@ -27,6 +27,7 @@ import com.github.a2g.core.objectmodel.SentenceItem;
 import com.google.gwt.touch.client.Point;
 import com.github.a2g.core.primitive.A2gException;
 
+/** @brief This interface lists all the methods you string together as a big return object from either @ref IOnEntry or @ref IOnDoCommand */
 public interface IChainRootForScene extends IChainRootCommon
 , ISetVisible
 , ISetAnimationAsObjectInitial
@@ -48,10 +49,14 @@ public interface IChainRootForScene extends IChainRootCommon
 , ISetInventoryItemVisible 
 , ISleep
 {
-	// all the ChainEndActions can only be the last
-	// action in a chain.
-	// subroutine is an exeption since there is both a ChainEndAction
-	// and ChainableAction version of it.
+	
+	/**   @name End-of-chain-only methods that return ChainEndActions 
+	 *  These do something uncontinuable, and so they've been
+	 *  made to return ChainEndAction, which has no methods on it.
+	 * The exception here is  "subroutine" which has both a ChainEndAction
+	 *   and ChainableAction version of it.
+	 */
+	//@{
 	public ChainEndAction doBoth(ChainableAction a, ChainableAction b);
 	public ChainEndAction activateDialogTreeMode(int branchId);
 	public ChainEndAction onDoCommand(IGameScene scene, IOnDoCommand api,
@@ -59,21 +64,30 @@ public interface IChainRootForScene extends IChainRootCommon
 			SentenceItem itemB, double x, double y) throws A2gException;
 	public ChainEndAction subroutine(ChainEndAction orig);
 	public ChainEndAction switchTo(String sceneName, int arrivalSegment);
-	public ChainEndAction walkTo(double x, double y);// should not have ocode here because switching implies main character
-	public ChainEndAction walkTo(Point point);
+	//@}
 	
-	// there's 2 choices
-	// 1) BaseAction walkSwitch and ChainAbleAction walkTo
-	// 2) BaseAction walkTo, and ChainableAction walkWithNoSwitch
-	// client is likely to code with walkTo
-	// in case 1, using walkTo in its wrong scenario (as
-	// a terminator) would not be picked up by compiler.
-	// since a chainableAction would pass as a baseAction.
-	// in case 2, using walkTo in its wrong scenario (in
-	// the middle of a chain) where client expects it not
-	// to switch, WOULD generate a compiler error.
-	// Thus 2 is better, because it protects the user
-	// against unexpected results.
+	
+	/** @name End-of-chain-only Walk using edge detection 
+	 * These use the scene walker so need to otid 
+	 */
+	//@{
+	public ChainEndAction walkTo(double x, double y); 
+	public ChainEndAction walkTo(Point point);
+	//@}
+
+	/** @name End-of-chain-only Walk, but no edge detection 
+	 *  These all take a parameter for the "sceneName" that they switch to upon termination.
+	 */
+	//@{
+	public ChainEndAction walkAlwaysSwitch(double x, double y, String sceneName, int arrivalSegment) throws A2gException ;
+	public ChainEndAction walkAlwaysSwitch(Point point, String sceneName, int arrivalSegment) throws A2gException ;
+	public ChainEndAction walkAndScaleAlwaysSwitch(short ocode, Point p, double startScale, double endScale, String sceneName, int arrivalSegment) throws A2gException;
+	//@}
+	
+	/**  @name Walk with no edge detection
+	 * All the methods from this point on can be positioned in the start, end, or middle of method chains.
+     */
+	//@{
 	public ChainableAction walkNeverSwitch(double x, double y);
 	public ChainableAction walkNeverSwitch(Point point);
 	public ChainableAction walkNeverSwitchNonBlocking(short ocode, double x, double y);
@@ -81,82 +95,122 @@ public interface IChainRootForScene extends IChainRootCommon
 	public ChainableAction walkNeverSwitch(short ocode, Point point);
 	public ChainableAction walkAndTalkNeverSwitch(short ocode, double x, double y, String speech);
 	public ChainableAction walkAndScaleNeverSwitch(short ocode, Point p, double startScale, double endScale);
-	public ChainEndAction walkAlwaysSwitch(double x, double y, String sceneName, int arrivalSegment) throws A2gException ;
-	public ChainEndAction walkAlwaysSwitch(Point point, String string, int arrivalSegment) throws A2gException ;
-	public ChainEndAction walkAndScaleAlwaysSwitch(short ocode, Point p, double startScale, double endScale, String sceneName, int arrivalSegment) throws A2gException;
+	//@}
 	
-	public ChainableAction subroutine(ChainableAction orig);
 
-	public ChainableAction doNothing();
-
-	// plain..
+	/** @name Play animation methods starting with plain.. */
+	//@{
 	public ChainableAction playAnimation(String animationCode);
+	//@}
 
-	// simple backwards
+	/** @name ..simple backwards ...*/
+	//@{
 	public ChainableAction playAnimationBackwards(String animationCode);
+	//@}
 
-	// simple hold last frame
+	/** @name ..simple hold last frame ... */
+	//@{
 	public ChainableAction playAnimationHoldLastFrame(String animationCode);
+	//@}
 
-	// simple non blocking
+	/** @name ... simple non blocking ... */
+	//@{
 	public ChainableAction playAnimationNonBlocking(String animationCode);
+	//@}
 
-	// double combo1of3: backwards + hold last frame
-	public ChainableAction playAnimationBackwardsHoldLastFrame(
-			String animationCode);
+	/** @name ... double combo1of3: backwards + hold last frame ... */
+	//@{
+	public ChainableAction playAnimationBackwardsHoldLastFrame(String animationCode);
+	//@}
 
-	// double combo2of3: backwards + nonblocking
+	/** @name ... double combo2of3: backwards + nonblocking ... */
+	//@{
 	public ChainableAction playAnimationBackwardsNonBlocking(String animationCode);
+	//@}
+	
+	/** @name ... double combo2of3: holdLastFrame + nonblocking ... */
+	//@{
+	public ChainableAction playAnimationHoldLastFrameNonBlocking(String animationCode);
+	public ChainableAction playAnimationNonBlockingHoldLastFrame(String animationCode);
+	//@}
+	
+    /** @name ..and one method with the whole lot! */
+	//@{
+	public ChainableAction playAnimationBackwardsHoldLastFrameNonBlocking(String animationCode);
+	//@}
 
-	// double combo2of3: holdLastFrame + nonblocking
-	public ChainableAction playAnimationHoldLastFrameNonBlocking(
-			String animationCode);
-	public ChainableAction playAnimationNonBlockingHoldLastFrame(
-			String animationCode);
+
+	 
+	/** @name Play commands that don't take animation ids*/
+	//@{
+	public ChainableAction playSound(String stid);
+	public ChainableAction playSoundNonBlocking(String stid);
+	/**  play as opposed to setTitleCard in the direct api */
+	public ChainableAction playTitleCard(String text, double durationInSecs);
+	//@}
 	
-	// ..and one method with the whole lot!
-	public ChainableAction playAnimationBackwardsHoldLastFrameNonBlocking(
-			String animationCode);
-	
-	
-	// talking methods
+	/** @name Talk methods */
+	//@{
 	public ChainableAction talk(String animCode, String speech);
 	public ChainableAction talk(String speech);
 	public ChainableAction talkWithoutIncrementingFrame(String animCode, String speech);
 	public ChainableAction talkWithoutIncrementingFrameNonBlocking(String animCode, String speech);
 	public ChainableAction talkWithoutIncrementingFrame(String speech);
 	public ChainableAction talkWithoutIncrementingFrameNonBlocking(String speech);
+	//@}
 	
-	// move whilst animating - less animation frame hungry way of moving
+	/** @name Move-whilst-animating - less animation frame hungry way of moving */
+	//@{
 	public ChainableAction moveWhilstAnimating(short objId, double x, double y);
 	public ChainableAction moveWhilstAnimatingInY(short objId, double y);
 	public ChainableAction moveWhilstAnimatingNonBlocking(short objId, double x,double y);
 	public ChainableAction moveWhilstAnimatingLinearNonBlocking(short objId, double x,double y);
 	public ChainableAction moveWhilstAnimatingLinear(short objId, double x, double y);
+	//@}
 	
-	// move camera..
+	/** @name  Move camera... */
+	//@{
 	public ChainableAction moveCameraToNewXPosition(double x,double durationInSecs);
 	public ChainableAction moveCameraToNewXPositionNonBlocking(double x,double durationInSecs);
 	public ChainableAction moveCameraToNewYPosition(double y,double durationInSecs);
 	public ChainableAction moveCameraToNewYPositionNonBlocking(double y,double durationInSecs);
+	//@}
 	
-	// I added "look" a new verb that just takes the first frame of the animation
+	/** @name Look */
+	//@{
+	/** I added "look" a new verb that just takes the first frame of the animation*/
 	public ChainableAction look(String atid, double duration);
+	//@}
 	
-	// play as opposed to setTitleCard in the direct api
-	public ChainableAction playTitleCard(String text, double durationInSecs);
-	
-	// other play commands
-	public ChainableAction playSound(String stid);
-	public ChainableAction playSoundNonBlocking(String stid);
-			
-	// commands that seem direct, but all have a temporal aspect
+	/** @name Commands that seem direct, but all have a temporal aspect */
+	//@{
 	@Override public ChainableAction sleep(int milliseconds);
 	public ChainableAction swapVisibility(short ocodeA, short ocodeB);
 	public ChainableAction waitForFrame(short ocode, int frame);
-
-	// ones that really should be in the direct api
+	//@}
+	
+	/** @name Direct api candidates! 
+	 * these should really be in the direct api */
+	//@{
 	public ChainableAction hideAll();
 	public ChainableAction setToInitialPosition(short ocode) ;
+	//@}
+	
+	/** @name Utility methods */
+	//@{
+	public ChainableAction subroutine(ChainableAction orig);
+	public ChainableAction doNothing();
+	//@}
+	
 
+/** @name Direct API commands
+ *  All the methods in the interfaces below are also available.
+ *  The methods are named the same as the interfaces they are included within.
+ *  They are done like this so that when the Direct API methods change, these change too. 
+ */
+///@{
+	@Override
+	public ChainableAction 	setVisible(short ocode, boolean visible);
+///@}
+ 
 }
